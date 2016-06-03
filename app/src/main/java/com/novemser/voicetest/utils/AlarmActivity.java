4a4c1821c847286832aeaaa9027721697fe59d5e
Novemser <2072806652@qq.com>
2016-06-03 19:16:45 +0800

    Alarm clock module, app start module added

--- app/src/main/java/com/novemser/voicetest/utils/AlarmActivity.java
+++ app/src/main/java/com/novemser/voicetest/utils/AlarmActivity.java
@@ -12,7 +12,6 @@ public class AlarmActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        SharedPreferences sharedPreferences = getSharedPreferences("user", MODE_PRIVATE);
 
         new AlertDialog.Builder(AlarmActivity.this).setTitle("提醒")
                 .setMessage("闹钟响了")
