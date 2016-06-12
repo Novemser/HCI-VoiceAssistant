6d30a96ea74704b186835c71a271e0795e1a0376
Novemser <2072806652@qq.com>
2016-06-12 12:57:55 +0800

    FAB removed

--- app/src/main/java/com/novemser/voicetest/actions/AlarmListsActivity.java
+++ app/src/main/java/com/novemser/voicetest/actions/AlarmListsActivity.java
@@ -1,17 +1,13 @@
 package com.novemser.voicetest.actions;
 
-import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 
 import com.novemser.voicetest.MainActivity;
 import com.novemser.voicetest.R;
@@ -32,15 +28,6 @@ public class AlarmListsActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         SQLiteDatabase database = MainActivity.db;
         List<AlarmItemSetting> itemSettings = new ArrayList<>();
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "乌拉拉~点我没用的~", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         Cursor cursor = database.rawQuery("select * from alarm", null);
         while (cursor.moveToNext()) {
